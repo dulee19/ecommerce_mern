0ce78bf9f01f4f7113c18026252836552a780913
dulee19 <adusan53@gmail.com>
2021-01-11 20:03:03 +0100

    reducers,action

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 // Entry point for the server
 import express from "express";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -11,19 +13,20 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("hello");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find(p => p._id === req.params.id);
+app.use(notFound);
 
-  res.json(product);
-});
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
